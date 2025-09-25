b999df148b74de733b05aa2b7004ef67f9322a41
Max Froumentin <github@e97.me>
2025-09-25 17:07:38 +0100

    Remove gccc.security.gov.uk records
    This subdomain is not currently in use but we are receiving security
    alerts about it, so this commit removes all records associated

--- dns/r53_0.7-gc3-staging-security-gov-uk.tf
+++ dns/r53_0.7-gc3-staging-security-gov-uk.tf
@@ -12,15 +12,3 @@ resource "aws_route53_record" "gc3-staging-delegated-zone" {
     "ns-1586.awsdns-06.co.uk."
   ]
 }
-
-#resource "aws_route53_record" "gccc-ds" {
-#  zone_id         = aws_route53_zone.sec-gov-uk.zone_id
-#  allow_overwrite = true
-#  name            = "gccc"
-#  ttl             = local.standard_ttl
-#  type            = "DS"
-
-#  records = [
-#    "16448 13 2 xxx"
-#  ]
-#}
